a2971ff9f270ac31ed6318a111012a69a2c2f94a
Your Name <your-email@example.com>
2025-04-25 03:03:57 +0530

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-console.log("connected");
+// console.log("connected");
 socket.on('signalingMessage', (message) => {
    socket.broadcast.emit("signalingMessage",message)
 })
